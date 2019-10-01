f1a86e7a51a9c4d487f5685f571dfcd714f3308c
Tyler Merritt <tgmerritt@gmail.com>
2019-10-01 16:19:55 -0500

    Update Uneeq-js and routes / views

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var getToken = require('../singleUseToken');
 var handleTranscript = require('../orchestrationLayer');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  async function awaitSingleUseToken() {
-    var token = await getToken.getSingleUseToken();
-    console.log(token);
-    res.render('index', {
-      title: 'Sophie - A Digital Human',
-      singleUseToken: token
-    });
-  }
-
-  awaitSingleUseToken();
+  res.render('index', {
+    title: 'Sophie - A Digital Human in Node.js'
+  });
 });
 
 // This isn't ever hit because we integrate directly to Watson via the UneeQ Admin Console - but this is what it would look like.
@@ -22,7 +14,7 @@ router.post('/transcript', function (req, res, next) {
   // console.log('body = ' + req.body);
   // The second param for processPostRequest really just acts as the value in a switch statement, we can re-write this to send 'watson' or 'dialogflow' as a string to the underlying call to NLP
   // In the future, it would be best to dynamically pass in which NLP we should query in the POST itself, providing customers flexibility 
-  handleTranscript.processPostRequest(req.body, '/api/public/v1/converse', (responseBody) => {
+  handleTranscript.processPostRequest(req.body, '/v2/{session=projects/*/agent/sessions/*}:detectIntent', (responseBody) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.write(responseBody);
